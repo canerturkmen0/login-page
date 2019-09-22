9e63152c681e96a814184311a2574e137f92f6a8
canerturkmen0 <caner98turkmen@gmail.com>
2019-09-22 11:34:44 +0300

    start

---
+++ login-page/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <h1>Hellooo</h1>
+    </div>
+  );
+}
+
+export default App;
